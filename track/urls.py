4e5340d573cf3dab970ca5568f0f9b4e69b41142
kess13 <138617196+kess13@users.noreply.github.com>
2024-08-15 21:22:21 +0200

    UPDATED EMPTY URL

--- track/urls.py
+++ track/urls.py
@@ -22,7 +22,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('members/', include('members.urls')),  # Custom member-related URLs
+    path('', include('members.urls')),  # Custom member-related URLs
 
 ]
 
