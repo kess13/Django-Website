bc9ac2309b0edf1077ffd42b47a928dce45b8b9c
kess13 <138617196+kess13@users.noreply.github.com>
2024-08-27 19:46:55 +0200

    null value allowed

--- members/models.py
+++ members/models.py
@@ -12,20 +12,20 @@ class Task(models.Model):
 
     # Existing fields
     user = models.ForeignKey(User, on_delete=models.CASCADE)  # Add this line to link Task to User
-    misto = models.CharField('Name', max_length=50)  # Field to 255 chars
+    misto = models.CharField('Name', max_length=50,null=True, blank=True)  # Field to 255 chars
     gruz = models.CharField('Description', max_length=10, choices=GRUZ_CHOICES)
-    litres = models.CharField('Litres', max_length=20)
-    price = models.CharField('Price', max_length=25)
-    massa = models.CharField('Massa', max_length=50)
-    punkt = models.CharField('Punkt', max_length=50)
+    litres = models.CharField('Litres', max_length=20,null=True, blank=True)
+    price = models.CharField('Price', max_length=25,null=True, blank=True)
+    massa = models.CharField('Massa', max_length=50,null=True, blank=True)
+    punkt = models.CharField('Punkt', max_length=50,null=True, blank=True)
     date = models.DateTimeField(default=timezone.now)
 
-    derzh_nomer = models.CharField('derzh_nomer', max_length=50)
-    ttn_nomer = models.CharField('ttn_nomer', max_length=50)
-    skilki_vantazhu = models.CharField('skilki_vantazhu', max_length=50)
-    odometr_vizd = models.CharField('odometr_vizd', max_length=50)
-    odometr_viezd = models.CharField('odometr_viezd', max_length=50)
-    zaprawka_time = models.CharField('zaprawka_time', max_length=50)
+    derzh_nomer = models.CharField('derzh_nomer', max_length=50,null=True, blank=True)
+    ttn_nomer = models.CharField('ttn_nomer', max_length=50,null=True, blank=True)
+    skilki_vantazhu = models.CharField('skilki_vantazhu', max_length=50,null=True, blank=True)
+    odometr_vizd = models.CharField('odometr_vizd', max_length=50,null=True, blank=True)
+    odometr_viezd = models.CharField('odometr_viezd', max_length=50,null=True, blank=True)
+    zaprawka_time = models.CharField('zaprawka_time', max_length=50,null=True, blank=True)
     zavantazhennya_datetime = models.DateTimeField('Дата и время завантаженя', null=True, blank=True)
     rozvantazhennya_datetime = models.DateTimeField('Дата и время розвантаження', null=True, blank=True)
     rozvantazhennya_mistse = models.CharField('Місце розвантаження', max_length=255, null=True, blank=True)
